e94dceaf53837327c2b17e8207642f408e37cd4e
Sunil Sharma <77779035+soulofadreamer@users.noreply.github.com>
2023-11-01 19:07:02 +0530

    updated

---
+++ ThePortfolio/Views/ExperienceView.swift
@@ -0,0 +1,18 @@
+//
+//  ExperienceView.swift
+//  ThePortfolio
+//
+//  Created by Sunil Sharma on 01/11/23.
+//
+
+import SwiftUI
+
+struct ExperienceView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExperienceView()
+}
---
+++ ThePortfolio/Views/MiddleView.swift
@@ -0,0 +1,18 @@
+//
+//  MiddleView.swift
+//  ThePortfolio
+//
+//  Created by Sunil Sharma on 01/11/23.
+//
+
+import SwiftUI
+
+struct MiddleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MiddleView()
+}
