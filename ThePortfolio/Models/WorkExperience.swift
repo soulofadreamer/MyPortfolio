f79fa478013a877c815f9004c7abaf9f3c2f1c54
Sunil Sharma <77779035+soulofadreamer@users.noreply.github.com>
2023-11-08 01:37:05 +0530

    All done with Experience view

--- ThePortfolio/Models/WorkExperience.swift
+++ ThePortfolio/Models/WorkExperience.swift
@@ -7,8 +7,8 @@
 
 import Foundation
 
-struct WorkExperience: Identifiable {
-    var id = UUID()
+struct WorkExperience: Identifiable, Decodable {
+    let id = UUID()
     var companyName: String
     var workedAs:  String
     var description: String
@@ -16,17 +16,6 @@ struct WorkExperience: Identifiable {
     var startDate: String
     var endDate: String
     var companyLogo: String
-    init(id: UUID = UUID(), companyName: String, workedAs: String, description: String, summary: String, startDate: String, endDate: String, companyLogo: String) {
-        self.id = id
-        self.companyName = companyName
-        self.workedAs = workedAs
-        self.description = description
-        self.summary = summary
-        self.startDate = startDate
-        self.endDate = endDate
-        self.companyLogo = companyLogo
-        
-    }
     
     
 }
